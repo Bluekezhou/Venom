731594698b90aa5a2546eae1cf4d521da80c6d75
bluecake <bluekezhou@qq.com>
2021-03-02 12:20:11 +0800

    fix: bufTerminal cannot process delete correctly

--- utils/terminal/terminal.go
+++ utils/terminal/terminal.go
@@ -858,9 +858,7 @@ func (bt *BufTerminal) TerminalEmu(input byte) (buffer []byte) {
 	switch input {
 	case 0x7f:
 		// 删除
-		if bt.pos > 1 {
-			bt.input = append(bt.input[:bt.pos-1], bt.input[bt.pos:]...)
-		}
+		bt.input = append(bt.input[:bt.pos], bt.input[bt.pos+1:]...)
 		bt.pos = max(bt.pos-1, -1)
 		bt.cursize = max(bt.cursize-1, 0)
 	// TODO: 支持HOME、END、左右方向键等操作
